1fe841c93bd7d2607fc2c74edca5e478b2c8c03a
DonaldY <448641125@qq.com>
2018-04-03 02:13:03 +0800

    update side-search plugin

---
+++ frontEnd/src/public-resource/components/side-menu/search/README.md
@@ -0,0 +1,11 @@
+### Side-menu-search-panel
+
+> #### 简介
+> 1. 用于配合Webpack进行开发，实现模块化，完全解耦
+> 2. 符合CommonJs，只需
+> ```
+> const sideSearch = require("side-search.js");
+> sideSearch.init();
+> sideSearch.showEvent();
+> ```
+
--- frontEnd/src/public-resource/components/side-menu/search/side-search.js
+++ frontEnd/src/public-resource/components/side-menu/search/side-search.js
@@ -2,37 +2,55 @@
 
 require('./side-search.css');
 
-var page = {
+var sideSearchPage = {
     init: function() {
 	this.bindEvent();
     },
 
     bindEvent: function() {
 
-	$('#mask-area').click(function() {
-	    $('.show_panel').css("width", "0");
-	   
-	});
+	$('#mask-area').click(this.hideEvent);
 	
-	$('#input-goods-search').keyup(function() {
-	    var text = $.trim($('#input-goods-search').val());
-	    if (text) {
-		$('#search-clear').show();
-	    } else {
-		$('#search-clear').hide();
-	    }
-	});
+	$('#input-goods-search').keyup(this.cleanInputEvent);
     },
 
-    open: function() {
+    showEvent: function() {
+	$('#search-panel').show();
 
+	$('#search-panel').css("opacity", "1");
+	$('#search-panel').css("transition", "opacity 0.4s linear 0.01s");
+
+	setTimeout(function() {
+	    $('.show_panel').css("left", "0");
+	}, 500);
+
+	setTimeout(function() {
+	    $('#search-panel').css("transition", "");
+	}, 1000);
     },
 
-    close: function() {
+    hideEvent: function() {
+	$('.show_panel').css("left", "-100%");
+	
+	setTimeout(function() {
+	    $('#search-panel').css("opacity", "0");
+	    $('#search-panel').css("transition", "opacity 0.4s linear 0.01s");
+	}, 500);
+	
+	setTimeout(function() {
+	    $('#search-panel').css("transition", "");
+	    $('#search-panel').hide();
+	}, 1300);
+    },
 
+    cleanInputEvent: function() {
+	var text = $.trim($('#input-goods-search').val());
+	if (text) {
+	    $('#search-clear').show();
+	} else {
+	    $('#search-clear').hide();
+	}
     }
 };
 
-$(function() {
-    page.init();
-});
+module.exports = sideSearchPage;
