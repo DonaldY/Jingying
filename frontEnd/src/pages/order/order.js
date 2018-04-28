34723d86f00d43dc593ac9bae56f82ac38bfa1a9
DonaldY <448641125@qq.com>
2018-04-28 19:28:18 +0800

    add order

---
+++ frontEnd/src/pages/order/order.js
@@ -0,0 +1,19 @@
+'use strict';
+
+require('../../public-resource/components/common/default.css');
+require('./order.css');
+require('../../public-resource/iconfont/font-style.css');
+
+var page = {
+  init: function() {
+    this.bindEvent();
+  },
+
+  bindEvent: function() {
+
+  },
+};
+
+$(function() {
+  page.init();
+});
